44f9235f6cdc43aa3dc3e64a9970ced88dac6d05
Senthil Kumar T <senthilktmz@outlook.com>
2025-01-01 02:06:15 +0530

    taking request from orchestrator

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ use std::sync::Arc;
 
 mod orchestrator;
 
-use orchestrator::orchestrator_routes;
+use orchestrator::server_util::ServerContext;
+use orchestrator::{orchestrator_routes};
 use runautils::actix_server_util::serve_requests;
+use uuid::Uuid;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -16,49 +18,41 @@ struct Args {
     port: String,
 }
 
-#[derive(Clone)]
-pub struct ServerExecDependencies<'a> {
-    pub http_request_decrypt_key: &'a [u8; 32],
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let args = Args::parse();
 
-    let dependencies = inject_dependencies().unwrap_or_else(|err| {
-        eprintln!("{:?}", err);
-        std::process::exit(1);
-    });
-
-    match inject_dependencies() {
-        Ok(dependencies) => dependencies,
+    let server_context = match get_server_context() {
+        Ok(context) => context,
         Err(e) => {
             println!("Unable to resolve dependencies");
             std::process::exit(1);
         }
     };
 
-    println!("Serving on: {}", &args.work_dir);
-    println!("Serving on: {}", &args.port);
-
     let work_dir = args.work_dir;
     let port = args.port;
 
     let routes = orchestrator_routes::routes();
-    serve_requests(routes, work_dir, port, dependencies).await
+    serve_requests(routes, work_dir, port, server_context).await
 }
 
 fn get_http_request_decrypt_key() -> &'static [u8; 32] {
     let test_key = &b"0123456789abcdef0123456789abcdef";
-    test_key
+    return test_key;
 }
 
-fn inject_dependencies() -> Result<Arc<Box<dyn Any + Send + Sync>>, String> {
-    let dependencies: Arc<Box<dyn Any + Send + Sync>> =
-        Arc::new(Box::new(ServerExecDependencies {
-            http_request_decrypt_key: get_http_request_decrypt_key(),
-        }));
-    Ok(dependencies)
+fn get_server_context() -> Result<Arc<Box<dyn Any + Send + Sync>>, String> {
+    let server_context: Arc<Box<dyn Any + Send + Sync>> = Arc::new(Box::new(ServerContext {
+        http_request_decrypt_key: get_http_request_decrypt_key(),
+        state_storage_map: Default::default(),
+        server_execution_instance_uuid: generate_uuid(),
+    }));
+    Ok(server_context)
+}
+
+fn generate_uuid() -> String {
+    Uuid::new_v4().to_string()
 }
 //
 //
--- src/orchestrator/mod.rs
+++ src/orchestrator/mod.rs
@@ -4,4 +4,4 @@ pub mod orchestrator_routes;
 pub mod task_agent;
 pub mod ws_handle_task_request;
 pub mod payload_util;
-mod server_util;
+pub mod server_util;
